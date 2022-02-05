da69c1980ab36e1a8231ee00c1b34a071334ae22
Marcos <marcosvto1@gmail.com>
2022-02-05 18:27:18 -0300

    feat: add findAll with paginate to customer repository

--- src/provider/database/postgree/customer/customer.repository.ts
+++ src/provider/database/postgree/customer/customer.repository.ts
@@ -1,10 +1,43 @@
-import { Customer as CustomerPersistency } from "./customer.entity";
 import { getConnection } from 'typeorm';
-import { FindCustomerById, ICustomerRepository, SaveCustomer, UpdateCustomer } from "@/domain/repositories/customer";
+import { Customer as CustomerPersistency } from "./customer.entity";
+import { FindAllCustomer, FindCustomerById, ICustomerRepository, SaveCustomer, UpdateCustomer } from "@/domain/repositories/customer";
+
+enum Paginate {
+  MAX_PER_PAGE=1,
+  DEFAULT_PAGE=1
+}
 
 export class CustomerRepository implements ICustomerRepository {
+
+  async findAllCustomer(dto: FindAllCustomer.Input): Promise<FindAllCustomer.Output> {
+    const repository = getConnection('default').getRepository(CustomerPersistency);
+
+    const page = dto.page ? dto.page : Paginate.DEFAULT_PAGE;
+    const take = dto.pageSize ? dto.pageSize : Paginate.MAX_PER_PAGE;
+    const skip = (page - 1) * take;
+
+    const result = await repository.find({
+      take,
+      skip
+    });
+
+    const countAll = await repository.count();
+
+    const totalPages = Math.ceil(countAll / take);
+
+    return {
+      records: result,
+      meta: {
+        page: page,
+        pageSize: take,
+        totalPages,
+        total: countAll
+      }
+    }
+  }
+
   async findCustomerById(id: number): Promise<FindCustomerById.Output> {
-    const repository = getConnection('default').getRepository(CustomerPersistency)
+    const repository = getConnection('default').getRepository(CustomerPersistency);
     const result = await repository.findOne(id);
     if (result) {
       return {
@@ -14,7 +47,7 @@ export class CustomerRepository implements ICustomerRepository {
   }
 
   async updateCustomer(dto: UpdateCustomer.Input): Promise<UpdateCustomer.Output> {
-    const repository = getConnection('default').getRepository(CustomerPersistency)
+    const repository = getConnection('default').getRepository(CustomerPersistency);
 
     const result = await repository.update(dto.id, {
       name: dto.name,
@@ -30,7 +63,7 @@ export class CustomerRepository implements ICustomerRepository {
   }
   
   async emailExists(email: string): Promise<boolean> {
-    const repository = getConnection('default').getRepository(CustomerPersistency)
+    const repository = getConnection('default').getRepository(CustomerPersistency);
     const result = await repository.findOne({
       email
     });
@@ -38,7 +71,7 @@ export class CustomerRepository implements ICustomerRepository {
   }
   
   async saveCustomer({ name , email }: SaveCustomer.Input): Promise<SaveCustomer.Output> {
-    const repository = getConnection('default').getRepository(CustomerPersistency)
+    const repository = getConnection('default').getRepository(CustomerPersistency);
     const customerInstance = repository.create({
       name,
       email

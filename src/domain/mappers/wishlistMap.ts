4c3ab74e6541ceace0711e717ce7f8c3254f76bb
Marcos <marcosvto1@gmail.com>
2022-02-06 19:25:51 -0300

    refactor: enrich data mapper

--- src/domain/mappers/wishlistMap.ts
+++ src/domain/mappers/wishlistMap.ts
@@ -1,15 +1,29 @@
+import { IProduct, Product } from "@/domain/entities/product";
 import { IWishlist, Wishlist } from "@/domain/entities/wishlist";
+
+export type IWishlistWithProduct = IWishlist & Omit<IProduct, "id">
 
 export class WishlistMap {
   public static toResponse(wishlistItem: Wishlist) {
     return {
       id: wishlistItem.id,
-      productId: wishlistItem.productId,
-      customerId: wishlistItem.customerId
+      productId: wishlistItem.product.id,
+      customerId: wishlistItem.customerId,
+      title: wishlistItem.product.title,
+      price: wishlistItem.product.price,
+      image: wishlistItem.product.image,
+      reviewScore: wishlistItem.product.reviewScore
     }
   }
 
-  public static toDomain(dto: IWishlist) {
-    return new Wishlist(dto.id, dto.productId, dto.customerId);
+  public static toDomain(dto: IWishlistWithProduct) {
+    const product = new Product(
+      dto.productId,
+      dto.title,
+      dto.price,
+      dto.image,
+      dto.reviewScore
+    );
+    return new Wishlist(dto.id, dto.productId, dto.customerId, product);
   }
 }

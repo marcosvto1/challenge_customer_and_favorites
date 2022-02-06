42da4f80c2ddc71e19399b439691bc1607856dee
Marcos <marcosvto1@gmail.com>
2022-02-06 19:26:43 -0300

    refactor: enrich entity

--- src/domain/entities/product.ts
+++ src/domain/entities/product.ts
@@ -1,21 +1,50 @@
 import { Contract } from "@/core";
 import { Entity } from "@/shared/entities/entity";
 
+export interface IProduct {
+  id: string;
+  title: string;
+  image: string;
+  price: number;
+  reviewScore?: number;
+}
 export class Product extends Entity {
+
   constructor(
-    private readonly title: string,
-    private readonly price: number,
-    private readonly image: string,
-    private readonly id: string,
-    private readonly reviewScore: number,
+    private readonly _id: string,
+    private readonly _title: string,
+    private readonly _price: number,
+    private readonly _image: string,
+    private readonly _reviewScore?: number,
   ) {
     super();
 
     this.addNotifications([
-        new Contract().requires(title, "Product.title", "Title obrigatório")
-        .requires(price, "Product.price", "Preço Obrigatório")
-        .requires(id, "Product.id", "Id Obrigatório")
+        new Contract().requires(_title, "Api.Product.title", "Title obrigatório")
+        .requires(_price, "Api.Product.price", "Preço Obrigatório")
+        .requires(_image, "Api.Product.image", "Image obrigatório")
+        .requires(_id, "Api.Product.id", "Id Obrigatório")
       ]
     );
+  }
+
+  public get id(): string {
+    return this._id;
+  }
+
+  public get title(): string {
+    return this._title;
+  }
+
+  public get image(): string {
+    return this._image;
+  }
+
+  public get price(): number {
+    return this._price;
+  }
+  
+  public get reviewScore(): number | undefined {
+    return this._reviewScore;
   }
 }

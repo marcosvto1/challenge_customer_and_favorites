09eed7543f8fd0acd7b406c3f3b4590103ede51b
Marcos <marcosvto1@gmail.com>
2022-02-06 17:08:27 -0300

    feat: add entity

---
+++ src/domain/entities/wishlist.ts
@@ -0,0 +1,37 @@
+import { Contract, Notifiable } from "@/core";
+
+export interface IWishlist {
+  id: string;
+  productId: string;
+  customerId: number;
+}
+
+export class Wishlist extends Notifiable implements IWishlist {
+  constructor(
+    private _id: string,
+    private _productId: string,
+    private _customerId: number
+  ) {
+    super();
+
+    this.addNotifications([
+      new Contract().requires(_id, 'id', 'the field :id is required')
+      .requires(_productId, 'id', 'the field :productId is required')
+      .requires(_customerId, 'id', 'the customerId is required')
+      .isGUID(_id, 'id', 'the field :id is invalid guid value')
+      .isGUID(_productId, 'productId', 'the field productId is invalid guid value')
+    ])
+  }
+
+  public get id(): string {
+    return this._id
+  }
+
+  public get productId(): string {
+    return this._productId
+  }
+
+  public get customerId(): number {
+    return this._customerId;
+  }
+}

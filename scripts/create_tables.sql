33c59aa9cd0b46660857319a4369f9855b6b9463
Marcos <marcosvto1@gmail.com>
2022-02-06 12:11:50 -0300

    chore: add more script to users table

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -7,3 +7,18 @@ CREATE TABLE IF NOT EXISTS customers (
   name VARCHAR(250) NOT NULL,
   email VARCHAR(250) NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(250) NOT NULL,
+  email VARCHAR(250) NOT NULL,
+  password VARCHAR(250) NOT NULL
+);
+
+
+INSERT 
+	INTO users 
+    (name, email, password) 
+    VALUES ('administrador', 'admin@admin.com', '$2b$12$jMfmJpofJYeiT7UqwBWtse.7leFfM8Mr9lfFUtfVkuj/C.MkF5BQq');
+
+

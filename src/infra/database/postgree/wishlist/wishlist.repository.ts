618d9af28725f92f1cfeb60087a6123ad6d0e89d
Marcos <marcosvto1@gmail.com>
2022-02-06 19:28:58 -0300

    feat: add wishlist repository

---
+++ src/infra/database/postgree/wishlist/wishlist.repository.ts
@@ -0,0 +1,24 @@
+import { IWishlistRepository, SaveProductInWishlist } from "@/domain/repositories/wishlist";
+import { Wishlist } from "@/infra/database/postgree/wishlist/wishlist.entity";
+import { getConnection } from "typeorm";
+
+export class WishlistRepository implements IWishlistRepository {
+  async saveProductInWhishlist (params: SaveProductInWishlist.Input): Promise<SaveProductInWishlist.Output> {
+    const repository = getConnection('default').getRepository(Wishlist);
+    const wishlistCreate = repository.create(params);
+    const wishlist = await repository.save(wishlistCreate);
+    return wishlist;
+  }
+
+  async productAlreadyExistsInWishlist(productId: string, customerId: number): Promise<boolean> {
+    const repository = getConnection('default').getRepository(Wishlist);
+
+    const result = await repository.findOne({
+      where: {
+        productId,
+        customerId
+      }
+    });
+    return !!result === true;
+  }
+}

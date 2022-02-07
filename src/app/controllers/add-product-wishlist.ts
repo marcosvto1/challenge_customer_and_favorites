87ed04c83a863c3b3d289da800028593b5f70595
Marcos <marcosvto1@gmail.com>
2022-02-06 21:01:42 -0300

    feat: add middleware auth

--- src/app/controllers/add-product-wishlist.ts
+++ src/app/controllers/add-product-wishlist.ts
@@ -1,13 +1,16 @@
+import { Request, Response } from "express";
+import { ClassMiddleware, Controller, Post } from "@overnightjs/core";
+import { middleware } from "@/app/middlewares";
 import { AddProductWishlistInput } from "@/domain/usecases/whitelist/add-product.input";
 import { UserCase } from "@/shared/usecases/usecase";
-import { Controller, Post } from "@overnightjs/core";
-import { Request, Response } from "express";
 
 @Controller('customers')
+@ClassMiddleware(middleware.enableAuth())
 export class AddProductWhitelistController {
   constructor(
     private readonly useCase: UserCase
   ) {}
+  
   @Post(':id/wishlist')
   public async addProductWhitelistController(req: Request, res: Response) {
     const { id } = req.params;

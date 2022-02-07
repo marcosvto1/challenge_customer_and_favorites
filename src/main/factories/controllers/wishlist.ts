f4824e4082f170efa1b9d2ffbea8feb98b191106
Marcos <marcosvto1@gmail.com>
2022-02-06 21:02:42 -0300

    feat: add factory to GetProducts

--- src/main/factories/controllers/wishlist.ts
+++ src/main/factories/controllers/wishlist.ts
@@ -1,8 +1,11 @@
-import { AddProductWhitelistController } from "@/app/controllers/add-product-whitelist"
+import { AddProductWhitelistController } from "@/app/controllers/add-product-wishlist"
+import { GetProductInWishlistController } from "@/app/controllers/get-product-in-wishlist"
 import { makeAddProductInWishlist } from "@/main/factories/usecases/add-product-in-wishlist"
+import { makeGetProductsInWishlist } from "@/main/factories/usecases/get-product-in-wishlist"
 
 export const makeWishlistControllers = () => {
   return  [
     new AddProductWhitelistController(makeAddProductInWishlist()),
+    new GetProductInWishlistController(makeGetProductsInWishlist())
   ]
 }

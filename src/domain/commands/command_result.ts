363036b061a4bb8ac2ba98abc651e3392933a0ce
Marcos <marcosvto1@gmail.com>
2022-02-06 21:00:36 -0300

    refactor: remove unsed files

--- src/domain/commands/command_result.ts
+++
@@ -1,8 +0,0 @@
-import { ICommand } from "@/shared/commands/command";
-import { ICommandResult } from "@/shared/commands/command_result";
-
-export class CommandResult implements ICommandResult {
-  constructor(
-    public readonly success: boolean, 
-    private readonly message: any) {}
-}
--- src/domain/commands/create_customer.command.ts
+++
@@ -1,7 +0,0 @@
-import { ICommand } from "@/shared/commands/command";
-
-export class CreateCustomerCommand implements ICommand {
-  validate(): void {
-    throw new Error("Method not implemented.");
-  }
-}

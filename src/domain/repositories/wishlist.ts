cd3a1d1727c325e2fa168e620019f1c355ef2752
Marcos <marcosvto1@gmail.com>
2022-02-06 21:02:15 -0300

    feat: add contract to FindProducts

--- src/domain/repositories/wishlist.ts
+++ src/domain/repositories/wishlist.ts
@@ -4,10 +4,15 @@ import { IWishlist } from "@/domain/entities/wishlist"
 export interface IWishlistRepository {
   saveProductInWhishlist: (params: SaveProductInWishlist.Input) => Promise<SaveProductInWishlist.Output>;
   productAlreadyExistsInWishlist: (productId: string, customerId: number) => Promise<boolean>;
+  findProducts: (customerId: number) => Promise<FindProducts.Output>
 }
 
 export namespace SaveProductInWishlist {
   export type Input = Omit<IWishlist, "id"> & Omit<IProduct, "id">;
 
   export type Output = IWishlist & Omit<IProduct, "id"> | undefined
+}
+
+export namespace FindProducts {
+  export type Output = {records: Array<IWishlist & Omit<IProduct, "id">>}
 }

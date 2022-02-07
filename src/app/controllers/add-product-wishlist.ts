e4518f8d0331de4d4386c21768b31bc301292f37
Marcos <marcosvto1@gmail.com>
2022-02-07 00:26:41 -0300

    refactor: move rate limiter middleware to a middleware method

--- src/app/controllers/add-product-wishlist.ts
+++ src/app/controllers/add-product-wishlist.ts
@@ -1,17 +1,18 @@
 import { Request, Response } from "express";
-import { ClassMiddleware, Controller, Post } from "@overnightjs/core";
+import { ClassMiddleware, Controller, Middleware, Post } from "@overnightjs/core";
 import { middleware } from "@/app/middlewares";
 import { AddProductWishlistInput } from "@/domain/usecases/whitelist/add-product.input";
 import { UserCase } from "@/shared/usecases/usecase";
 
 @Controller('api/customers')
-@ClassMiddleware([middleware.enableAuth(), middleware.enableRateLimiter()])
+@ClassMiddleware(middleware.enableAuth())
 export class AddProductWhitelistController {
   constructor(
     private readonly useCase: UserCase
   ) {}
   
   @Post(':id/wishlist')
+  @Middleware(middleware.enableRateLimiter())
   public async addProductWhitelistController(req: Request, res: Response) {
     const { id } = req.params;
     const { productId } = req.body;
